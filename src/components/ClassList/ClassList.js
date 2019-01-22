1b6bf1577f3f0d55d3da87e987c4a2ceec115d6b
Your Name <you@example.com>
2019-01-22 15:06:57 -0700

    All Done

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => {
+        console.log(res)
+        this.setState({ students: res.data })
+      })
   }
 
   render() {
+    let mapper = this.state.students.map((student, i) => {
+      return <Link to={`/student/${student.id}`} key={i}> <h3>{student.first_name}{" "}{student.last_name}</h3></Link>
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mapper}
       </div>
     )
   }
